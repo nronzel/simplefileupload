510bfbea057d18a972614b3fbe7010188129a428
nronzel <86695181+nronzel@users.noreply.github.com>
2024-01-05 17:07:24 -0500

    chore: fixed func name
    no need for capital, not being exported

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ import (
 
 func setupTestServer() (*FileServer, string) {
 	tempDir, _ := os.MkdirTemp("", "testuploads")
-	fileServer := NewFileServer(tempDir)
+	fileServer := newFileServer(tempDir)
 	return fileServer, tempDir
 }
 
